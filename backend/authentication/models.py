776023e9be9d86e8cf8cb1f4df054dfa77ee2df1
Ldelilah <ruthtirusew944@gmail.com>
2022-07-20 14:37:00 +0300

    Authentication app created

---
+++ backend/authentication/models.py
@@ -0,0 +1,31 @@
+from pyexpat import model
+from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
+# Create your models here.
+
+class user_manager(UserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('Users must have an username')
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        user = self.create_user(username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class user(AbstractUser):
+    role_options=("store_manager", "Store Manager"),("store_staff", "Store Staff"),("admin", "Admin"),("finance", "Finance"),("general_manager","General manager"),("purchaser", "Purchaser")
+    department_options=("finance", "Finance"),("purchasing", "Purchasing"),("management", "Management"), ("human_resources", "Human Resources")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    role = models.CharField(max_length=30, choices=role_options)
+    department=models.CharField(max_length=30, choices=department_options)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
