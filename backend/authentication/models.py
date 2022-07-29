3cc1af11b422a4b39a9d8d59a18a40ba16e3f00c
Yididya <jedidiahamdualem@gmail.com>
2022-07-29 15:41:10 +0300

    created user groups and permissions

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,6 +1,8 @@
 from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
+from  .permissions import *
+
 # Create your models here.
 
 class user_manager(UserManager):
@@ -29,3 +31,5 @@ class user(AbstractUser):
 
     def __str__(self):
         return self.username
+
+create_group()
